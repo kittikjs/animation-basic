f91f7b9676886bd152ab542b05f3e26c2f0124ea
ghaiklor <ghaiklor@gmail.com>
2015-12-21 18:55:23 +0200

    test(animation): Add tests

--- test/unit/Basic.test.js
+++ test/unit/Basic.test.js
@@ -1,8 +1,73 @@
 import { assert } from 'chai';
 import sinon from 'sinon';
+import Rectangle from 'kittik-shape-rectangle';
 import Animation from '../../src/Basic';
 
 describe('Animation::Basic', () => {
+  it('Should properly export easing', () => {
+    let animation = new Animation();
+    assert.isNumber(animation.EASING.inQuad(0, 0, 0, 0));
+    assert.isNumber(animation.EASING.outQuad(0, 0, 0, 0));
+    assert.isNumber(animation.EASING.inOutQuad(0, 0, 0, 100));
+    assert.isNumber(animation.EASING.inOutQuad(0, 0, 0, 0));
+    assert.isNumber(animation.EASING.inCubic(0, 0, 0, 0));
+    assert.isNumber(animation.EASING.outCubic(0, 0, 0, 0));
+    assert.isNumber(animation.EASING.inOutCubic(0, 0, 0, 100));
+    assert.isNumber(animation.EASING.inOutCubic(0, 0, 0, 0));
+    assert.isNumber(animation.EASING.inQuart(0, 0, 0, 0));
+    assert.isNumber(animation.EASING.outQuart(0, 0, 0, 0));
+    assert.isNumber(animation.EASING.inOutQuart(0, 0, 0, 100));
+    assert.isNumber(animation.EASING.inOutQuart(0, 0, 0, 0));
+    assert.isNumber(animation.EASING.inQuint(0, 0, 0, 0));
+    assert.isNumber(animation.EASING.outQuint(0, 0, 0, 0));
+    assert.isNumber(animation.EASING.inOutQuint(0, 0, 0, 100));
+    assert.isNumber(animation.EASING.inOutQuint(0, 0, 0, 0));
+    assert.isNumber(animation.EASING.inSine(0, 0, 0, 0));
+    assert.isNumber(animation.EASING.outSine(0, 0, 0, 0));
+    assert.isNumber(animation.EASING.inOutSine(0, 0, 0, 0));
+    assert.isNumber(animation.EASING.inExpo(0, 0, 0, 0));
+    assert.isNumber(animation.EASING.inExpo(5, 0, 0, 0));
+    assert.isNumber(animation.EASING.outExpo(0, 0, 0, 0));
+    assert.isNumber(animation.EASING.outExpo(5, 0, 0, 0));
+    assert.isNumber(animation.EASING.inOutExpo(0, 0, 0, 0));
+    assert.isNumber(animation.EASING.inOutExpo(2, 0, 0, 2));
+    assert.isNumber(animation.EASING.inOutExpo(1, 0, 0, 100));
+    assert.isNumber(animation.EASING.inOutExpo(100, 0, 0, 0));
+    assert.isNumber(animation.EASING.inCirc(0, 0, 0, 0));
+    assert.isNumber(animation.EASING.outCirc(0, 0, 0, 0));
+    assert.isNumber(animation.EASING.inOutCirc(0, 0, 0, 100));
+    assert.isNumber(animation.EASING.inOutCirc(0, 0, 0, 0));
+    assert.isNumber(animation.EASING.inElastic(0, 0, 0, 0));
+    assert.isNumber(animation.EASING.inElastic(5, 0, 0, 5));
+    assert.isNumber(animation.EASING.inElastic(5, 0, -2, 2));
+    assert.isNumber(animation.EASING.inElastic(5, 0, 2, 2));
+    assert.isNumber(animation.EASING.outElastic(0, 0, 0, 0));
+    assert.isNumber(animation.EASING.outElastic(5, 0, 0, 5));
+    assert.isNumber(animation.EASING.outElastic(5, 0, -2, 2));
+    assert.isNumber(animation.EASING.outElastic(5, 0, 2, 2));
+    assert.isNumber(animation.EASING.inOutElastic(0, 0, 0, 0));
+    assert.isNumber(animation.EASING.inOutElastic(8, 0, 0, 8));
+    assert.isNumber(animation.EASING.inOutElastic(8, 0, -2, 2));
+    assert.isNumber(animation.EASING.inOutElastic(8, 0, 2, 2));
+    assert.isNumber(animation.EASING.inOutElastic(0.5, 0, 2, 2));
+    assert.isNumber(animation.EASING.inBack(0, 0, 0, 0));
+    assert.isNumber(animation.EASING.outBack(0, 0, 0, 0));
+    assert.isNumber(animation.EASING.inOutBack(0, 0, 0, 0));
+    assert.isNumber(animation.EASING.inOutBack(1, 0, 0, 5));
+    assert.isNumber(animation.EASING.inBounce(0, 0, 0, 0));
+    assert.isNumber(animation.EASING.outBounce(1, 0, 0, 5));
+    assert.isNumber(animation.EASING.outBounce(0.7, 0, 0, 0));
+    assert.isNumber(animation.EASING.outBounce(0.8, 0, 0, 0.001));
+    assert.isNumber(animation.EASING.inOutBounce(0, 0, 0, 0));
+    assert.isNumber(animation.EASING.inOutBounce(1, 0, 0, 5));
+  });
+
+  it('Should properly create animation with custom options', () => {
+    let animation = new Animation({duration: 2000, easing: 'outExpo'});
+    assert.equal(animation.getDuration(), 2000);
+    assert.equal(animation.getEasing(), 'outExpo');
+  });
+
   it('Should properly get/set from options object', () => {
     let animation = new Animation();
     assert.notOk(animation.get('enabled'));
@@ -12,59 +77,40 @@ describe('Animation::Basic', () => {
     assert.notOk(animation.get('foo.bar'), false);
   });
 
-  it('Should properly enable animation', () => {
+  it('Should properly get/set duration of the animation', () => {
     let animation = new Animation();
-    assert.instanceOf(animation.enable(), Animation);
-    assert.ok(animation.get('enabled'));
+    assert.equal(animation.getDuration(), 1000);
+    assert.instanceOf(animation.setDuration(2000), Animation);
+    assert.equal(animation.getDuration(), 2000);
   });
 
-  it('Should properly disable animation', () => {
+  it('Should properly get/set easing of the animation', () => {
     let animation = new Animation();
-    assert.instanceOf(animation.disable(), Animation);
-    assert.notOk(animation.get('enabled'));
+    assert.equal(animation.getEasing(), 'outQuad');
+    assert.instanceOf(animation.setEasing('inQuad'), Animation);
+    assert.equal(animation.getEasing(), 'inQuad');
   });
 
-  it('Should properly check if animation is enabled', () => {
+  it('Should properly throw error if not supported easing', () => {
     let animation = new Animation();
-    assert.notOk(animation.isEnabled());
-    assert.instanceOf(animation.enable(), Animation);
-    assert.ok(animation.isEnabled());
-  });
-
-  it('Should properly check if animation is disabled', () => {
-    let animation = new Animation();
-    assert.ok(animation.isDisabled());
-    assert.instanceOf(animation.enable(), Animation);
-    assert.notOk(animation.isDisabled());
+    assert.throws(() => animation.setEasing('wrong'), Error, 'Unknown easing: wrong');
   });
 
   it('Should properly throw error when animate is not implemented', () => {
     let animation = new Animation();
-    assert.throws(() => animation.animate(), Error, 'animate() method must be implemented');
+    assert.throws(() => animation.animate(), Error, 'You must implement animate() method');
   });
 
-  it('Should properly pipe chunk to the next one item in the chain when animation is disabled', () => {
+  it('Should properly animate property in shape', done => {
     let animation = new Animation();
-    let cb = sinon.spy();
+    let shape = new Rectangle();
+    let mock = sinon.mock(shape);
 
-    animation._transform('test', null, cb);
+    mock.expects('set').atLeast(60);
 
-    assert.ok(animation.isDisabled());
-    assert.isNull(cb.getCall(0).args[0]);
-    assert.equal(cb.getCall(0).args[1], 'test');
-  });
-
-  it('Should properly call animate method when animation is enabled', () => {
-    let animation = new Animation();
-    let mock = sinon.mock(animation);
-    let cb = sinon.spy();
-
-    mock.expects('animate').once();
-
-    assert.instanceOf(animation.enable(), Animation);
-    assert.ok(animation.isEnabled());
-    animation._transform('test', null, cb);
-
-    mock.verify();
+    animation.animateProperty({shape: shape, property: 'x'}).on('end', () => {
+      mock.verify();
+      done();
+    });
   });
 });
