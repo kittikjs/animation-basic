41fd29508df4af1e5a4ffc6d40d24f80a682de01
ghaiklor <ghaiklor@gmail.com>
2016-02-05 18:32:54 +0200

    test(animation): Refactor tests

--- test/unit/Animation.test.js
+++ test/unit/Animation.test.js
@@ -5,13 +5,13 @@ import Animation from '../../src/Animation';
 
 describe('Animation::Basic', () => {
   it('Should properly create animation with custom options', () => {
-    let animation = new Animation({duration: 2000, easing: 'outExpo'});
+    const animation = new Animation({duration: 2000, easing: 'outExpo'});
     assert.equal(animation.getDuration(), 2000);
     assert.equal(animation.getEasing(), 'outExpo');
   });
 
   it('Should properly get/set from options object', () => {
-    let animation = new Animation();
+    const animation = new Animation();
     assert.notOk(animation.get('enabled'));
     assert.instanceOf(animation.set('foo.bar', true), Animation);
     assert.ok(animation.get('foo.bar'));
@@ -20,35 +20,35 @@ describe('Animation::Basic', () => {
   });
 
   it('Should properly get/set duration of the animation', () => {
-    let animation = new Animation();
+    const animation = new Animation();
     assert.equal(animation.getDuration(), 1000);
     assert.instanceOf(animation.setDuration(2000), Animation);
     assert.equal(animation.getDuration(), 2000);
   });
 
   it('Should properly get/set easing of the animation', () => {
-    let animation = new Animation();
+    const animation = new Animation();
     assert.equal(animation.getEasing(), 'outQuad');
     assert.instanceOf(animation.setEasing('inQuad'), Animation);
     assert.equal(animation.getEasing(), 'inQuad');
   });
 
   it('Should properly throw error if not supported easing', () => {
-    let animation = new Animation();
+    const animation = new Animation();
     assert.throws(() => animation.setEasing('wrong'), Error, 'Unknown easing: wrong');
   });
 
   it('Should properly make delay', done => {
-    let animation = new Animation();
+    const animation = new Animation();
     animation.delay(1).then(() => {
       done();
     });
   });
 
   it('Should properly trigger onTick method when animates', done => {
-    let animation = new Animation();
-    let shape = new Rectangle();
-    let mock = sinon.mock(animation);
+    const animation = new Animation();
+    const shape = new Rectangle();
+    const mock = sinon.mock(animation);
 
     mock.expects('emit').atLeast(1).withArgs('tick');
 
@@ -59,9 +59,9 @@ describe('Animation::Basic', () => {
   });
 
   it('Should properly trigger onEasing method when animates', done => {
-    let animation = new Animation();
-    let shape = new Rectangle();
-    let mock = sinon.mock(animation);
+    const animation = new Animation();
+    const shape = new Rectangle();
+    const mock = sinon.mock(animation);
 
     mock.expects('onEasing').atLeast(1);
 
@@ -72,7 +72,7 @@ describe('Animation::Basic', () => {
   });
 
   it('Should properly throw error when animate() is not implemented', done => {
-    let animation = new Animation();
+    const animation = new Animation();
 
     animation.animate().catch(error => {
       assert.instanceOf(error, Error);
@@ -82,9 +82,9 @@ describe('Animation::Basic', () => {
   });
 
   it('Should properly animate property in shape', done => {
-    let animation = new Animation({duration: 1});
-    let shape = new Rectangle();
-    let mock = sinon.mock(shape);
+    const animation = new Animation({duration: 1});
+    const shape = new Rectangle();
+    const mock = sinon.mock(shape);
 
     mock.expects('set').atLeast(1);
 
@@ -95,7 +95,7 @@ describe('Animation::Basic', () => {
   });
 
   it('Should properly serialize animation to the object', () => {
-    let animation = new Animation();
+    const animation = new Animation();
 
     assert.deepEqual(animation.toObject(), {
       type: 'Animation',
@@ -107,12 +107,12 @@ describe('Animation::Basic', () => {
   });
 
   it('Should properly serialize animation to the JSON', () => {
-    let animation = new Animation();
+    const animation = new Animation();
     assert.equal(animation.toJSON(), '{"type":"Animation","options":{"duration":1000,"easing":"outQuad"}}');
   });
 
   it('Should properly create Animation instance from static create()', () => {
-    let animation = Animation.create({duration: 1});
+    const animation = Animation.create({duration: 1});
     assert.equal(animation.get('duration'), 1);
     assert.instanceOf(animation, Animation);
   });
@@ -122,12 +122,12 @@ describe('Animation::Basic', () => {
   });
 
   it('Should properly throw exception if object representation is not from this animation', () => {
-    let obj = {type: 'Slide', options: {}};
+    const obj = {type: 'Slide', options: {}};
     assert.throws(() => Animation.fromObject(obj), Error, 'Slide is not an object representation of the Animation');
   });
 
   it('Should properly create Animation instance from object representation', () => {
-    let obj = {
+    const obj = {
       type: 'Animation',
       options: {
         duration: 1,
@@ -135,15 +135,15 @@ describe('Animation::Basic', () => {
       }
     };
 
-    let animation = Animation.fromObject(obj);
+    const animation = Animation.fromObject(obj);
     assert.instanceOf(animation, Animation);
     assert.equal(animation.getDuration(), 1);
     assert.equal(animation.getEasing(), 'inExpo');
   });
 
   it('Should properly create Animation instance from JSON representation', () => {
-    let json = '{"type":"Animation","options":{"duration":1000,"easing":"outQuad"}}';
-    let animation = Animation.fromJSON(json);
+    const json = '{"type":"Animation","options":{"duration":1000,"easing":"outQuad"}}';
+    const animation = Animation.fromJSON(json);
 
     assert.instanceOf(animation, Animation);
     assert.equal(animation.getDuration(), 1000);
