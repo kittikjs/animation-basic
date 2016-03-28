bc19760ce2de891697405cd1fe01c516bd9c4c2b
ghaiklor <ghaiklor@gmail.com>
2016-03-28 14:57:45 +0300

    docs(animation): Update examples

--- examples/advanced.js
+++ examples/advanced.js
@@ -1,23 +1,23 @@
 "use strict";
 
-const cursor = require('kittik-cursor').create().resetTTY().hideCursor();
+const cursor = require('kittik-cursor').create().reset().hideCursor();
 const Rectangle = require('kittik-shape-rectangle');
 const Animation = require('../lib/Animation');
 const shapes = [
-  Rectangle.create({background: 'dark_blue', x: 'left', y: 'top', text: 'Shape 1'}),
-  Rectangle.create({background: 'dark_blue', x: 'center', y: 'top', text: 'Shape 2'}),
-  Rectangle.create({background: 'dark_blue', x: 'right', y: 'top', text: 'Shape 3'}),
-  Rectangle.create({background: 'navy_blue', x: 'left', y: 'middle', text: 'Shape 4'}),
-  Rectangle.create({background: 'navy_blue', x: 'center', y: 'middle', text: 'Shape 5'}),
-  Rectangle.create({background: 'navy_blue', x: 'right', y: 'middle', text: 'Shape 6'}),
-  Rectangle.create({background: 'yellow_1', x: 'left', y: 'bottom', text: 'Shape 7'}),
-  Rectangle.create({background: 'yellow_1', x: 'center', y: 'bottom', text: 'Shape 8'}),
-  Rectangle.create({background: 'yellow_1', x: 'right', y: 'bottom', text: 'Shape 9'})
+  Rectangle.create(cursor, {background: 'blue', x: 'left', y: 'top', text: 'Shape 1'}),
+  Rectangle.create(cursor, {background: 'blue', x: 'center', y: 'top', text: 'Shape 2'}),
+  Rectangle.create(cursor, {background: 'blue', x: 'right', y: 'top', text: 'Shape 3'}),
+  Rectangle.create(cursor, {background: 'green', x: 'left', y: 'middle', text: 'Shape 4'}),
+  Rectangle.create(cursor, {background: 'green', x: 'center', y: 'middle', text: 'Shape 5'}),
+  Rectangle.create(cursor, {background: 'green', x: 'right', y: 'middle', text: 'Shape 6'}),
+  Rectangle.create(cursor, {background: 'yellow', x: 'left', y: 'bottom', text: 'Shape 7'}),
+  Rectangle.create(cursor, {background: 'yellow', x: 'center', y: 'bottom', text: 'Shape 8'}),
+  Rectangle.create(cursor, {background: 'yellow', x: 'right', y: 'bottom', text: 'Shape 9'})
 ];
 
 // Your animation
 class Slide extends Animation {
-  animate(shape, cursor) {
+  animate(shape) {
     return Promise.all([
       this.animateProperty({shape: shape, property: 'x', startValue: Math.random() * 100 - 50, endValue: shape.getX()}),
       this.animateProperty({shape: shape, property: 'y', startValue: Math.random() * 30 - 15, endValue: shape.getY()})
@@ -43,7 +43,7 @@ const nextShape = shape => {
 };
 
 const playAnimation = index => {
-  animation.animate(shapes[index], cursor).then(nextShape);
+  animation.animate(shapes[index]).then(nextShape);
 };
 
 const animation = new Slide({duration: 2000}).on('tick', onTick);
--- examples/simple.js
+++ examples/simple.js
@@ -1,11 +1,11 @@
 "use strict";
 
-const cursor = require('kittik-cursor').create().resetTTY().hideCursor();
-const shape = require('kittik-shape-rectangle').create({background: 'white', x: 'center'});
+const cursor = require('kittik-cursor').create().reset().hideCursor();
+const shape = require('kittik-shape-rectangle').create(cursor, {background: 'white', x: 'center'});
 const Animation = require('../lib/Animation');
 
 class Slide extends Animation {
-  animate(shape, cursor) {
+  animate(shape) {
     return new Promise(resolve => {
       this
         .animateProperty({shape: shape, property: 'x', startValue: -shape.getWidth(), endValue: shape.getX()})
@@ -14,4 +14,4 @@ class Slide extends Animation {
   }
 }
 
-new Slide({duration: 2000}).on('tick', shape => shape.render(cursor) && cursor.flush().eraseScreen()).animate(shape).then(() => cursor.showCursor().flush());
+new Slide({duration: 2000}).on('tick', shape => shape.render() && cursor.flush().eraseScreen()).animate(shape).then(() => cursor.showCursor().flush());
